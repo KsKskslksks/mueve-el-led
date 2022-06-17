ce99fcdd3b13d3932f9f237420ad964313996851
KsKskslksks <107255636+KsKskslksks@users.noreply.github.com>
2022-06-17 13:48:24 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,47 @@
+let Y = 0
+basic.forever(function () {
+    let X = 0
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (0 < 4) {
+            led.unplot(X, Y)
+            Y += 1
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (0 < 4) {
+            led.unplot(X, Y)
+            Y += 1
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (0 < 4) {
+            led.unplot(X, Y)
+            Y += 1
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (0 < 4) {
+            led.unplot(X, Y)
+            Y += 1
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (0 < 4) {
+            led.unplot(X, Y)
+            Y += 1
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+    }
+})
+basic.forever(function () {
+	
+})
